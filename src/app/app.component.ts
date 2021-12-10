f34b540516490ab7e43c152a8995a466645cfd46
Martin Mwangi <mwangimartin1904@gmail.com>
2021-12-10 15:19:43 +0300

    Track by to deal with large list for performance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,26 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-
 export class AppComponent {
   // viewMode = 'map'
   courses = [
-    {id:1, name:"Course 1"},
-    {id:2, name:"Course 2"},
-    {id:3, name:"Course 3"}
-  ]
+    { id: 1, name: 'Course 1' },
+    { id: 2, name: 'Course 2' },
+    { id: 3, name: 'Course 3' },
+  ];
 
-
-  onAdd(){
-    const id = this.courses.length+1;
-    this.courses.push({id, name:`Course ${id}`})
+  onAdd() {
+    const id = this.courses.length + 1;
+    this.courses.push({ id, name: `Course ${id}` });
   }
-  
-  onRemove(course: { id: number; name: string; }){
+
+  onRemove(course: { id: number; name: string }) {
     let index = this.courses.indexOf(course);
     this.courses.splice(index, 1);
   }
-
+  
+  trackCourse(index: number, course: { id: number, name: string }) {
+    course ? course.id : undefined;
+  }
 }
