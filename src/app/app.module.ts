d6415fed8f126ce7a1cc3d9e5f399e97bfcdd93e
Martin Mwangi <mwangimartin1904@gmail.com>
2021-12-15 11:30:05 +0300

    Ng Model inputs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
 @NgModule({
   declarations: [
     AppComponent,
+    ContactFormComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/contact-form/contact-form.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'contact-form',
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.css']
+})
+export class ContactFormComponent {
+  log(x: any){
+    console.log("Heeeyeeeeee");
+    
+    console.log(x);
+    
+  }
+
+}
