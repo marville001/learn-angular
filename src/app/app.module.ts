fa8c64cebe1b99845a1fdbbd1f978861a2d09db4
Martin Mwangi <mwangimartin1904@gmail.com>
2021-12-15 10:58:03 +0300

    New Branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SummaryPipe } from './pipes/summary.pipe';
-import { FavoriteComponent } from './components/favorite/favorite.component';
-import { CoursesComponent } from './components/courses/courses.component';
-import { TitleCasePipe } from './pipes/title-case.pipe';
-import { InputFormatDirective } from './input-format.directive';
-import { ZippyComponent } from './components/zippy/zippy.component';
-
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent,
-    SummaryPipe,
-    FavoriteComponent,
-    TitleCasePipe,
-    InputFormatDirective,
-    ZippyComponent
   ],
   imports: [
     BrowserModule,
