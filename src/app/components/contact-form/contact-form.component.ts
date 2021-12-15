b62876c3723eb4f516067810ed92c639417afbd7
Martin Mwangi <mwangimartin1904@gmail.com>
2021-12-15 12:05:31 +0300

    Handling validations

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -7,10 +7,11 @@ import { Component } from '@angular/core';
 })
 export class ContactFormComponent {
   log(x: any){
-    console.log("Heeeyeeeeee");
-    
     console.log(x);
-    
+  }
+
+  submit(form: any){
+    console.log(form);
   }
 
 }
